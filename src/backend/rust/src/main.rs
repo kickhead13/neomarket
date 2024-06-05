eaa2e77c9c51d34346fa0bc68ee4b5aa14546a6b
kickhead13 <ana.alexandru.gabriel@proton.me>
2024-06-05 16:00:23 +0300

    backend/tls: added TLS/SSL for backend (same .crt and .key as before)

--- src/backend/rust/src/main.rs
+++ src/backend/rust/src/main.rs
@@ -1,6 +1,7 @@
 use actix_web;
 use actix_cors;
 use local_ip_address;
+use std::io::Read;
 
 mod api;
 mod db;
@@ -10,15 +11,30 @@ mod encryption;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     if let Ok(lan_ip) = local_ip_address::local_ip() {
+ 
+
+        env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+        let mut builder = openssl::ssl::SslAcceptor::mozilla_intermediate(openssl::ssl::SslMethod::tls()).unwrap();
+
+        builder
+            .set_private_key(&load_encrypted_private_key())
+            .unwrap();
+
+        builder.set_certificate_chain_file("tls/cert.crt").unwrap();
+
+
         let mut lan_ip_str = format!("{:?}", lan_ip);
+        let lan_ip = lan_ip_str.clone();
         lan_ip_str.push_str(":8080");
         let mut https = format!("https://");
         https.push_str(&format!("{:?}", lan_ip));
         println!("api binding to {}...", lan_ip_str);
         
-        let _ = actix_web::HttpServer::new(move || 
+        let _ = actix_web::HttpServer::new(move || {
             actix_web::App::new()
-                .wrap(actix_cors::Cors::default().allowed_origin(&https))
+                //.wrap(actix_web::middleware::Logger::default())
+                //.wrap(actix_cors::Cors::default().allowed_origin(&https))
                 .service(api::get_test)
                 .service(api::fetch_user_password)
                 .service(api::sign_user_up)
@@ -29,8 +45,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 .service(api::fetch_prods_from_cat)
                 .service(api::send_email)
                 .service(api::check_user_password)
-            )
-            .bind(lan_ip_str)?
+            })
+            .bind_openssl(lan_ip_str, builder)?
+            .workers(2)
             .run()
             .await;
         Ok(())
@@ -38,4 +55,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         println!("api error: can't detect LAN ip.");
         Ok(())
     }
+}
+
+fn load_encrypted_private_key() -> openssl::pkey::PKey<openssl::pkey::Private> {
+    let mut file = std::fs::File::open("tls/cert.key").unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Failed to read file");
+
+    openssl::pkey::PKey::private_key_from_pem_passphrase(&buffer, b"password").unwrap()
 }
